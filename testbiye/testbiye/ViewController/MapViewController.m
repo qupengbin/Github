6b083c10c2354836c472e001dc288c635c5a90e5
qupengbin <qupengbin@gmail.com>
2014-05-07 17:13:45 +0800

    a
    a

--- testbiye/testbiye/ViewController/MapViewController.m
+++ testbiye/testbiye/ViewController/MapViewController.m
@@ -8,7 +8,12 @@
 
 #import "MapViewController.h"
 
-@interface MapViewController ()
+@interface MapViewController () <UIScrollViewDelegate>
+{
+    UIImageView *_mapImg;
+}
+
+@property(nonatomic,weak) IBOutlet UIScrollView *scroll;
 
 @end
 
@@ -30,6 +35,17 @@
     
     self.titlelab.text = @"乐 行 地 图";
 
+    self.scroll.minimumZoomScale = 1.0f;
+    self.scroll.maximumZoomScale = 50.0f;
+
+    UIImage *image = [UIImage imageNamed:@"comm_default.png"];
+    float scale = image.size.width/image.size.height;
+    float h = 320/scale;
+    
+    _mapImg = [[UIImageView alloc] initWithFrame:CGRectMake(0, (504-h)/2, 320, h)];
+    _mapImg.image = image;
+    [self.scroll addSubview:_mapImg];
+
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -43,6 +59,27 @@
 - (void)backBtnAction:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark UIScrollView delegate methods
+- (float)getZoomScale:(UIScrollView *)scrollView
+{
+    float imageWidth = _mapImg.frame.size.width;
+    float scrollWidth = scrollView.contentSize.width;
+    return scrollWidth/imageWidth;
+}
+
+//实现图片的缩放
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return _mapImg;
+}
+
+//实现图片在素芳过程中居中
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    CGFloat offsetX = (scrollView.bounds.size.width > scrollView.contentSize.width)?(scrollView.bounds.size.width - scrollView.contentSize.width)/2 : 0.0;
+    CGFloat offsetY = (scrollView.bounds.size.height > scrollView.contentSize.height)?(scrollView.bounds.size.height - scrollView.contentSize.height)/2 : 0.0;
+    _mapImg.center = CGPointMake(scrollView.contentSize.width/2 + offsetX,scrollView.contentSize.height/2 + offsetY);
 }
 
 @end
